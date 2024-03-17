7ef49fcef6ba4bc240cdde2a9114fd080de240bf
Pratyush Makkar <pratyushmakkar@gmail.com>
2024-03-17 01:56:40 -0400

    finished alu_execute wrapper

--- rtl/tb_wrappers/alu_execute_fsm_wrapper.sv
+++ rtl/tb_wrappers/alu_execute_fsm_wrapper.sv
@@ -1,4 +1,15 @@
-module alu_execute_fsm_wrapper();
+module alu_execute_fsm_wrapper (
+  input logic [31:0] i_operand_one,
+  input logic [31:0] i_operand_two,
+  input logic [31:0] alu_result_exp,
+  input logic carry_out_exp,
+  input logic [1:0] alu_op_sel,
+  input logic tx_ack_data,
+  output logic tx_finished,
+  output logic tb_error,
+  output logic clk,
+);
+
   logic clk;
   logic rst;
   logic [31:0] operand_one, operand_two, alu_result;
@@ -6,6 +17,7 @@ module alu_execute_fsm_wrapper();
   logic [1:0] alu_sel;
   logic carry_out, data_valid;
 
+  localparam MAX_ATTEMPS = 8;
   rv32_alu_fsm alu_fsm (
     .i_clk(clk),
     .i_rst(rst),
@@ -27,10 +39,20 @@ module alu_execute_fsm_wrapper();
     carry_out <= 'd0;
     data_valid <= 'd0;
     alu_result <= 'd0;
+    tb_error <= 1'b0;
+    tx_finished <= 1'b0;
     fork
-      toggleClock();
-      driveStimulus();
+      begin : toggle_clk_thread
+        toggleClock();
+      end
+      begin : drive_stim_thread
+        driveStimulus();
+      end
+      begin : tb_error_thread
+        @(posedge tb_error);
+      end
     join_any
+    disable fork;
   end
 
   task toggleClock;
@@ -38,7 +60,21 @@ module alu_execute_fsm_wrapper();
   endtask
 
   task driveStimulus();
-
+    forever begin
+      @(posedge clk);
+      operand_one <= i_operand_one;
+      operand_two <= i_operand_two;
+      alu_sel <= alu_op_sel;
+      stall_reset <= 1'b1;
+      fork 
+        @(posedge o_data_valid);
+        repeat (MAX_ATTEMPS) @(posedge clk);
+      join_any
+      if (!o_data_valid) tb_error <= 1'b1;
+      if (alu_result_exp != alu_result) tb_error <= 1'b1;
+      if (carry_out != carry_out_exp) tb_error <= 1'b1;
+      tx_finished <= 1'b1;
+      @(posedge tx_ack_data);
+    end
   endtask
-
 endmodule
