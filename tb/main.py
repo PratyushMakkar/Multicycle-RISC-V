58961bc19dd878d581071fc84d03610af513bbdd
Pratyush Makkar <pratyushmakkar@gmail.com>
2024-03-31 18:02:29 -0400

    Rewriting env

--- tb/main.py
+++ tb/main.py
@@ -1,11 +1,9 @@
+import os
+MY_TEST = os.environ['MY_TEST']
 
-import cocotb
-from cocotb.triggers import RisingEdge, FallingEdge
-from cocotb.types import LogicArray, Logic
-
-from RV32Components.registerFileData import RegisterFileData
-from RV32Components.registerFileData import RegisterFileDUT, RegisterFileDriver
-
-from tests.rv32_alu_fsm_test import rv32_alu_fsm_test
-from tests.rv32_instruction_fetch_test import rv32_instruction_fetch_sanity_test, rv32_instruction_repeated_test
+match MY_TEST:
+  case "INSTRUCTION_FETCH_STAGE":
+    from tests.rv32_instruction_fetch_test import rv32_instruction_fetch_sanity_test, rv32_instruction_repeated_test
+  case "ALU_EXECUTE_FSM":
+    from tests.rv32_alu_fsm_test import rv32_alu_fsm_test
 
