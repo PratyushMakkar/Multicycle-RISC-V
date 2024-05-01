539c052227720318ab06d713dd0410c908851e60
Pratyush Makkar <pratyushmakkar@gmail.com>
2024-05-01 00:40:24 -0300

    shifter-tested

--- rtl/core/instruction_execute/rv32I_multipler_shift_controlpath.sv
+++ rtl/core/instruction_execute/rv32I_multipler_shift_controlpath.sv
@@ -1,4 +1,7 @@
 module rv32I_multipler_shift_controlpath (
+  input logic i_clk,
+  input logic i_rst,
+
   input logic i_execute_en,
   input logic [2:0] i_execute_shifter_opcode,
   input logic [31:0] i_execute_operand_one,
@@ -20,43 +23,44 @@ logic [4:0] multiplier_shift;
 logic [63:0] multiplier_sign_extended;
 logic [16:0] multiplier_shift_index;
 
-assign multiplier_sign_extended = (i_execute_shifter_opcode == SLL) ? {32'h0, i_operand_one}
-                                : (i_execute_shifter_opcode == SRL) ? {32'h0, i_operand_one}
-                                : {{32{1'b1}}, i_operand_one};
+assign multiplier_sign_extended = (i_execute_shifter_opcode == 4'b0001) ? {32'h0, i_execute_operand_one}
+                                : (i_execute_shifter_opcode == 4'b0101) ? {32'h0, i_execute_operand_one}
+                                : {{32{1'b1}}, i_execute_operand_one};
 
-assign multiplier_shift_index = (multiplier_state_counter == 0) ? {{30{1'b0}}, multiplier_shift[0]};
-                              : (multiplier_state_counter == 1) ? {multiplier_shift[1], 2'h0}
-                              : (multiplier_state_counter == 2) ? {multiplier_shift[2], 4'h0}
-                              : (multiplier_state_counter == 3) ? {multiplier_shift[3], 8'h0}
-                              : {multiplier_shift[4], 16'h0};
+assign multiplier_shift_index = (multiplier_state_counter == 0) ? {multiplier_shift[0], ~multiplier_shift[0]}
+                              : (multiplier_state_counter == 1) ? {multiplier_shift[1], 1'h0, ~multiplier_shift[1]}
+                              : (multiplier_state_counter == 2) ? {multiplier_shift[2], 3'h0, ~multiplier_shift[2]}
+                              : (multiplier_state_counter == 3) ? {multiplier_shift[3], 7'h0, ~multiplier_shift[3]}
+                              : {multiplier_shift[4], 15'h0, ~multiplier_shift[4]};
 
                             
-enum {ShifterRst, ShifterStage, ShifterResultValid} shifter_state_e;
+enum {ShifterRst, ShifterStage, ShifterValid} shifter_state_e;
 logic [1:0] multiplier_phase_counter;
 logic [63:0] multiplier_accumulator;
 logic [63:0] multiplier_temp_register;
 
-always_ff @(posedge i_clk) begin : multipler_shifter_interface
+always_ff @(posedge i_clk) begin 
   o_execute_data_valid <= 1'b0;
+  o_execute_data_result <= 0;
 
-  if (i_execute_en) begin : shifter_circuit
+  if (i_execute_en) begin 
 
-    unique case (shifter_state_e) begin
-      ShifterRst: begin : shifter_rst
+    unique case (shifter_state_e)
+      ShifterRst: begin 
         multiplier_temp_register <= 0;
         multiplier_accumulator <= multiplier_sign_extended;
 
-        multiplier_shift <= (i_execute_shifter_opcode == SLL) ? i_execute_operand_two[4:0];
-                          : (i_execute_shifter_opcode == SRL) ? i_execute_operand_two[4:0];
+        multiplier_shift <= (i_execute_shifter_opcode == 4'b0001) ? i_execute_operand_two[4:0]
+                          : (i_execute_shifter_opcode == 4'b0101) ? i_execute_operand_two[4:0]
                           : ~i_execute_operand_two[4:0] + 1;
 
-        shifter_state_e <= ShifterQuartOne;
+        shifter_state_e <= ShifterStage;
         multiplier_state_counter <= 0;
         multiplier_phase_counter <= 0;
         o_multiplier_en <= 1'b0;
       end
 
-      ShifterStage: begin : shifter_stage
+      ShifterStage: begin 
         o_multiplier_en <= 1'b1;
         o_multiplier_operand_one <= (multiplier_phase_counter == 0) ? multiplier_accumulator[15:0]
                                     : (multiplier_phase_counter == 1) ? multiplier_accumulator[31:16]
@@ -69,7 +73,7 @@ always_ff @(posedge i_clk) begin : multipler_shifter_interface
           multiplier_temp_register <= (multiplier_phase_counter == 0) ? {32'd0, i_multiplier_result}
                                     : (multiplier_phase_counter == 1) ? multiplier_temp_register | {16'd0, i_multiplier_result, 16'd0}
                                     : (multiplier_phase_counter == 2) ? multiplier_temp_register | {i_multiplier_result, 32'd0}
-                                    :  {i_multiplier_result[15:0], 48'd0};
+                                    :  multiplier_temp_register | {i_multiplier_result[15:0], 48'd0};
 
           shifter_state_e <= (multiplier_phase_counter == 3) ? ShifterValid : ShifterStage;
           o_multiplier_en <= 1'b0;
@@ -77,7 +81,7 @@ always_ff @(posedge i_clk) begin : multipler_shifter_interface
         end
       end
 
-      ShifterValid: begin : shifter_valid
+      ShifterValid: begin 
         shifter_state_e <= ShifterStage;
         multiplier_phase_counter <= 0;
         multiplier_accumulator <= multiplier_temp_register;
@@ -86,16 +90,19 @@ always_ff @(posedge i_clk) begin : multipler_shifter_interface
 
         if (multiplier_state_counter == 4) begin 
           o_execute_data_valid <= 1'b1;
-          o_execute_data_result <= (i_execute_shifter_opcode == SLL) ? multiplier_temp_register[31:0]
+          o_execute_data_result <= (i_execute_shifter_opcode == 4'b0001) ? multiplier_temp_register[31:0]
                                   : multiplier_temp_register[63:32];
 
           shifter_state_e <= ShifterRst;
         end
-      end : shifter_valid
+      end 
+    endcase
 
-    end
-  end : shifter_circuit
+  end 
 
+  if (i_rst) begin
+    shifter_state_e <= ShifterRst;
+  end
 end
 
 endmodule
